5a61a44ddcb72d1304e7b996bb5c08617809e624
Hoang Tien <hoangtrungtien99@gmail.com>
2022-03-24 23:47:13 +0700

    Done

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const img = document.querySelectorAll("img")
+let goku = document.querySelector(".imgTooplip")
+
+const hanlderTooplip = (test) => {
+    for(let i = 0; i < img.length; i++) {
+        
+    }
+}
